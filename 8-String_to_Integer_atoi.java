064db6bd61220458f37e3137f0a3014804bc4637
chie <vaguelyweird@gmail.com>
2019-10-26 09:32:50 +0000

    optimizing char compare, remove trim & isDigit

--- 8-String_to_Integer_atoi.java
+++ 8-String_to_Integer_atoi.java
@@ -4,24 +4,32 @@ class Solution {
             return 0;
         }
 
-        str = str.trim();
         boolean isNegative = false;
-
-        if(str.length() != 0 && !Character.isDigit(str.charAt(0))) {
-            isNegative = str.charAt(0) == '-';
-            if(isNegative || str.charAt(0) == '+') {
-                str = str.substring(1);
-            }
-        }
+        boolean hasStarted = false;
 
         int number = 0;
 
         for (int i = 0; i < str.length(); i++) {
-            if (!Character.isDigit(str.charAt(i))) {
+            char c = str.charAt(i);
+            if (c < '0' || c > '9') {
+                if(!hasStarted) {
+                    if(c == ' ') {
+                        continue;
+                    }
+                    isNegative = c == '-';
+                    if(isNegative || c == '+') {
+                        hasStarted = true;
+                        continue;
+                    }
+                }
                 break;
             }
 
-            int digit = str.charAt(i) - '0';
+            if (!hasStarted) {
+                hasStarted = true;
+            }
+
+            int digit = c - '0';
             if ((Integer.MAX_VALUE - digit) / 10 < number) {
                 return isNegative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
             }
