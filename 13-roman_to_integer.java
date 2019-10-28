535bfcca08ef1e108a2c901f785bb50f00f03aa3
chie <vaguelyweird@gmail.com>
2019-10-28 10:45:39 +0000

    Readability edit

--- 13-roman_to_integer.java
+++ 13-roman_to_integer.java
@@ -2,75 +2,33 @@ class Solution {
     /**
      * Given a roman numeral, convert it to an integer. Input is guaranteed to be within the range from 1 to 3999.
      */
-    public int romanToInt(String s) {
-        // Naive solution
+     public int romanToInt(String s) {
         int num = 0;
-        for(int i = 0, j = 1, length = s.length(); i < length; i++) {
-            char c = s.charAt(i);
-            j = i + 1;
-            switch(c) {
-                case 'M':
-                    num += 1000;
-                    break;
-                case 'C':
-                    if(j < length) {
-                        char next = s.charAt(j);
-                        if(next == 'M') {
-                            num += 900;
-                            i++;
-                            break;
-                        }
-                        else if(next == 'D') {
-                            num += 400;
-                            i++;
-                            break;
-                        }
-                    }
-                    num += 100;
-                    break;
-                case 'D':
-                    num += 500;
-                    break;
-                case 'X':
-                    if(j < length) {
-                        char next = s.charAt(j);
-                        if(next == 'C') {
-                            num += 90;
-                            i++;
-                            break;
-                        }
-                        else if(next == 'L') {
-                            num += 40;
-                            i++;
-                            break;
-                        }
-                    }
-                    num += 10;
-                    break;
-                case 'L':
-                    num += 50;
-                    break;
-                case 'I':
-                    if(j < length) {
-                        char next = s.charAt(j);
-                        if(next == 'X') {
-                            num += 9;
-                            i++;
-                            break;
-                        }
-                        else if(next == 'V') {
-                            num += 4;
-                            i++;
-                            break;
-                        }
-                    }
-                    num++;
-                    break;
-                case 'V':
-                    num += 5;
-                    break;
+        for(int i = 0, length = s.length() - 1; i <= length; i++) {
+            int first = romanToInt(s.charAt(i));
+            if(i < length) {
+                int second = romanToInt(s.charAt(i + 1));
+                if(first <= second) {
+                    num += first < second ? second - first : first + second;
+                    i++;
+                    continue;
+                }
             }
+            num += first;
         }
         return num;
+    }
+
+    private int romanToInt(char c) {
+        switch(c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
     }
 }
