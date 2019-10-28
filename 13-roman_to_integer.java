3cfac3af6bba8d5a3f8ea7535c62e9261ba0915f
chie <vaguelyweird@gmail.com>
2019-10-28 12:31:20 +0000

    Revert "Ridiculously overbuilt enum method"
    This reverts commit 364389535b3476a3cbc685ae2791ead079922c38.

--- 13-roman_to_integer.java
+++ 13-roman_to_integer.java
@@ -2,44 +2,33 @@ class Solution {
     /**
      * Given a roman numeral, convert it to an integer. Input is guaranteed to be within the range from 1 to 3999.
      */
-
-     public static enum Roman {
-        I(1),
-        V(5),
-        X(10),
-        L(50),
-        C(100),
-        D(500),
-        M(1000);
-
-        private final int value;
-
-        Roman (final int aValue) {
-            value = aValue;
-        }
-
-        public int getValue() {
-            return value;
-        }
-    }
-
-     public int romanToInt(String str) {
+     public int romanToInt(String s) {
         int num = 0;
-        Roman[] arr = str.chars()
-                .mapToObj(c -> Roman.valueOf(String.valueOf((char) c)))
-                .toArray(Roman[]::new);
-        for(int i = 0, length = arr.length - 1; i <= length; i++) {
+        for(int i = 0, length = s.length() - 1; i <= length; i++) {
+            int first = romanToInt(s.charAt(i));
             if(i < length) {
-                int next = i+1;
-                int compare = arr[i].compareTo(arr[next]);
-                if(compare <= 0) {
-                    num += compare < 0 ? arr[next].getValue() - arr[i].getValue() : 2 * arr[i].getValue();
+                int second = romanToInt(s.charAt(i + 1));
+                if(first <= second) {
+                    num += first < second ? second - first : first + second;
                     i++;
                     continue;
                 }
             }
-            num += arr[i].getValue();
+            num += first;
         }
         return num;
+    }
+
+    private int romanToInt(char c) {
+        switch(c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
     }
 }
