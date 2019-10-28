c3057b3fdb8b9f057ade08e4c7585146c8a8a46c
chie <vaguelyweird@gmail.com>
2019-10-28 08:44:15 +0000

    Optimized for memory using preprocessing

--- 12-integer_to_roman.java
+++ 12-integer_to_roman.java
@@ -2,64 +2,12 @@ class Solution {
     /**
      * Given an integer, convert it to a roman numeral. Input is guaranteed to be within the range from 1 to 3999.
      */
-    public String intToRoman(int num) {
-        StringBuilder roman = new StringBuilder();
-        // Naive solution
-        while(num > 0) {
-            if(num >= 1000) {
-                roman.append('M');
-                num -= 1000;
-            }
-            else if(num >= 900) {
-                roman.append("CM");
-                num -= 900;
-            }
-            else if(num >= 500) {
-                roman.append('D');
-                num -= 500;
-            }
-            else if(num >= 400) {
-                roman.append("CD");
-                num -= 400;
-            }
-            else if(num >= 100) {
-                roman.append('C');
-                num -= 100;
-            }
-            else if(num >= 90) {
-                roman.append("XC");
-                num -= 90;
-            }
-            else if(num >= 50) {
-                roman.append('L');
-                num -= 50;
-            }
-            else if(num >= 40) {
-                roman.append("XL");
-                num -= 40;
-            }
-            else if(num >= 10) {
-                roman.append('X');
-                num -= 10;
-            }
-            else if(num == 9) {
-                roman.append("IX");
-                num -= 9;
-            }
-            else if(num >= 5) {
-                roman.append('V');
-                num -= 5;
-            }
-            else if(num == 4) {
-                roman.append("IV");
-                num = 0;
-            }
-            else {
-                roman.append('I');
-                num--;
-            }
-        }
+    public static String[] M = {"", "M", "MM", "MMM"};
+    public static String[] C = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+    public static String[] X = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    public static String[] I = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
 
-        return roman.toString();
+    public String intToRoman(int num) {
+        return M[num / 1000].concat(C[num % 1000 / 100]).concat(X[num % 100 / 10]).concat(I[num % 10]);
     }
 }
