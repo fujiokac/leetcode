753647ddc6635eea591355e470c6acee6388e65a
chie <vaguelyweird@gmail.com>
2019-10-29 17:01:32 +0000

    Divide & conquer solution using substring, 100% beats other solutions time & space

--- 14-longest_common_prefix.java
+++ 14-longest_common_prefix.java
@@ -3,18 +3,47 @@
  * If there is no common prefix, return an empty string "".
 */
 class Solution {
-    // Bottom up solution using substring & while, faster at the cost of space
+    /**
+     * Divide & conquer solution using substring
+     */
     public String longestCommonPrefix(String[] strs) {
-        if(strs == null || strs.length == 0) {
+        if (strs == null || strs.length == 0) {
             return "";
+        }
+        if (strs.length == 1) {
+            return strs[0];
         }
 
         String prefix = strs[0];
-        for(int i = 1; i < strs.length; i++){
-            String word = strs[i];
-            while(!word.startsWith(prefix)){
-                prefix = prefix.substring(0, prefix.length() - 1);
+        // Iterate through each word to narrow down prefix
+        for (int i = 1; i < strs.length; i++){
+            prefix = findPrefix(prefix, strs[i]);
+        }
+        return prefix;
+    }
+
+    /**
+     * Uses binary recursion to find prefix
+     */
+    private String findPrefix(String prefix, String word) {
+        if(prefix.isEmpty() || word.isEmpty()) {
+            return "";
+        }
+        if (prefix.length() == 1) {
+            return word.startsWith(prefix) ? prefix : "";
+        }
+
+        if (!word.startsWith(prefix)) {
+            // Find pivot
+            int mid = prefix.length() / 2;
+            // Check left half
+            String temp = findPrefix(prefix.substring(0, mid), word);
+            // If left half is prefix, check right half & append
+            if(temp.length() == mid) {
+                temp += findPrefix(prefix.substring(mid, prefix.length()), word.substring(mid, word.length()));
             }
+            // Save new prefix
+            prefix = temp;
         }
         return prefix;
     }
