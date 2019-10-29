4b375c73bb1ea9771d81f8f779d8591509ad4f03
chie <vaguelyweird@gmail.com>
2019-10-29 07:50:52 +0000

    More readable version of previous

--- 14-longest_common_prefix.java
+++ 14-longest_common_prefix.java
@@ -3,25 +3,29 @@
  * If there is no common prefix, return an empty string "".
 */
 class Solution {
-    // Naive solution
+    // More readable solution
     public String longestCommonPrefix(String[] strs) {
         StringBuilder prefix = new StringBuilder();
         for(int letter = 0; ; letter++) {
-            char c = ' ';
-            for(int word = 0; word < strs.length; word++) {
-                String current = strs[word];
-                if(current.length() <= letter || (word != 0 && c != current.charAt(letter))) {
-                    return prefix.toString();
-                }
-                if(word == 0) {
-                    c = current.charAt(letter);
-                }
-            }
-            if(c == ' ') {
+            char c = commonLetterAt(letter, strs);
+            if(c == 0) {
                 break;
             }
             prefix.append(c);
         }
         return prefix.toString();
+    }
+
+    private char commonLetterAt(int i, String[] strs) {
+        char c = 0;
+        for(int word = 0; word < strs.length; word++) {
+            if(strs[word].length() <= i || (word != 0 && c != strs[word].charAt(i))) {
+                return 0;
+            }
+            if(word == 0) {
+                c = strs[word].charAt(i);
+            }
+        }
+        return c;
     }
 }
