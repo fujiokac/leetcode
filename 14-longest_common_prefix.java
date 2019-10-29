4e773823e0d896cd7dbd16012fff9bd9c9eaca03
chie <vaguelyweird@gmail.com>
2019-10-29 09:31:43 +0000

    Top down attempt (takes longer than bottom up) -- test case dependent...

--- 14-longest_common_prefix.java
+++ 14-longest_common_prefix.java
@@ -3,7 +3,7 @@
  * If there is no common prefix, return an empty string "".
 */
 class Solution {
-    // Bottom up solution using substring instead of StringBuilder
+    // Top down solution using substring instead of StringBuilder
     public String longestCommonPrefix(String[] strs) {
         if(strs == null || strs.length == 0) {
             return "";
@@ -11,18 +11,11 @@ class Solution {
 
         String base = strs[0];
         int end = base.length();
-        for(int letter = end-1; letter > -1; letter--) {
+        for(int letter = 0; letter < end; letter++) {
             char c = base.charAt(letter);
-            for(int word = 1; word < strs.length; word++) {
-                if(!base.equals(word)) {
-                    int length = strs[word].length();
-                    if(length <= letter) {
-                        end = length;
-                        letter = length;
-                    }
-                    else if(c != strs[word].charAt(letter)) {
-                        end = letter;
-                    }
+            for(int word = 1; word < strs.length && strs[word].length() > letter; word++) {
+                if(letter == strs[word].length() || c != strs[word].charAt(letter)) {
+                    end = letter;
                 }
             }
         }
