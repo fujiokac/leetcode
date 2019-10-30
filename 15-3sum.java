3f81d1a5b6401f60d2c53f3f4281fdaf544ac084
chie <vaguelyweird@gmail.com>
2019-10-30 11:47:17 +0000

    Problem 15 too slow

---
+++ 15-3sum.java
@@ -0,0 +1,59 @@
+/**
+ * Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0?
+ * Find all unique triplets in the array which gives the sum of zero.
+ * Note: The solution set must not contain duplicate triplets.
+ */
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        if (nums == null || nums.length < 3) {
+            return Collections.emptyList();
+        }
+
+        if(nums.length == 3) {
+            if(nums[0] + nums[1] + nums[2] == 0) {
+                return new ArrayList() {{
+                    add(Arrays.asList(nums));
+                }};
+            }
+            return Collections.emptyList();
+        }
+
+        TreeSet<Integer> numSet = new TreeSet<>();
+        HashSet<Integer> duplicates = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if(!numSet.add(nums[i])) {
+                duplicates.add(nums[i]);
+            }
+        }
+
+        return findTriplets(numSet, duplicates);
+    }
+
+    private List<List<Integer>> findTriplets(Set<Integer> numSet, Set<Integer> duplicates) {
+        ArrayList<List<Integer>> triplets = new ArrayList<>();
+        Integer[] sorted = numSet.toArray(new Integer[numSet.size()]);
+        for (Iterator<Integer> it = numSet.iterator(); it.hasNext();) {
+            int first = it.next();
+            for (int i = 0, j = sorted.length-1; j > i;) {
+                if((first == sorted[i] || first == sorted[j]) && !duplicates.contains(first)) {
+                    continue;
+                }
+
+                int sum = first + sorted[i] + sorted[j];
+
+                if(sum == 0) {
+                    triplets.add(Arrays.asList(first, sorted[i], sorted[j]));
+                }
+                else if(sum > 0) {
+                    j--;
+                }
+                else if(sum < 0) {
+                    i++;
+                }
+            }
+        }
+
+        return triplets;
+    }
+}
