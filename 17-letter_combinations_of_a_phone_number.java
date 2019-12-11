868300b35da102085d917a4c6fac96beb764472d
chie <vaguelyweird@gmail.com>
2019-12-11 13:30:30 +0000

    First solution. Slow but memory efficient.

--- 17-letter_combinations_of_a_phone_number.java
+++ 17-letter_combinations_of_a_phone_number.java
@@ -14,7 +14,54 @@
  * 9: wxyz
 */
 class Solution {
-    public List<String> letterCombinations(String digits) {
+    public static final List<String> TWO = Arrays.asList("a","b","c");
+    public static final List<String> THREE = Arrays.asList("d","e","f");
+    public static final List<String> FOUR = Arrays.asList("g","h","i");
+    public static final List<String> FIVE = Arrays.asList("j","k","l");
+    public static final List<String> SIX = Arrays.asList("m","n","o");
+    public static final List<String> SEVEN = Arrays.asList("p","q","r","s");
+    public static final List<String> EIGHT = Arrays.asList("t","u","v");
+    public static final List<String> NINE = Arrays.asList("w","x","y","z");
+    public static final List<String> EMPTY = Collections.emptyList();
 
+    public List<String> letterCombinations(String digits) {
+        if (digits.length() == 0) {
+            return EMPTY;
+        }
+
+        return digitCombos(digits);
+    }
+
+    private List<String> digitCombos(String digits) {
+        char d = digits.charAt(0);
+
+        List<String> result = new ArrayList<String>();
+        List<String> letters = digitToLetters(d);
+
+        if(digits.length() == 1) {
+            result = letters;
+        }
+        else {
+            result = digitCombos(digits.substring(1)).stream()
+                .flatMap(s -> letters.stream()
+                        .map(l -> l+s)
+                )
+                .collect(Collectors.toList());
+        }
+        return result;
+    }
+
+    public List<String> digitToLetters(char digit) {
+        switch(digit) {
+            case '2': return TWO;
+            case '3': return THREE;
+            case '4': return FOUR;
+            case '5': return FIVE;
+            case '6': return SIX;
+            case '7': return SEVEN;
+            case '8': return EIGHT;
+            case '9': return NINE;
+            default: return EMPTY;
+        }
     }
 }
