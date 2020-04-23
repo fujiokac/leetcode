d8437b7b86bf8b7cfdfa52de23d3ee16e159e7e9
chie <vaguelyweird@gmail.com>
2020-04-23 10:41:55 +0000

    24 recursive simple solution

---
+++ 24-swap_nodes_in_pairs.java
@@ -0,0 +1,24 @@
+/**
+ * Given a linked list, swap every two adjacent nodes and return its head.
+ *
+ * You may not modify the values in the list's nodes, only nodes itself may be changed.
+ *
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode second = head.next;
+        head.next = swapPairs(second.next);
+        second.next = head;
+
+        return second;
+    }
+}
