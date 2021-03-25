ae685f1c39051ae7e16462fe046135210b4fbbad
fujiokac <vaguelyweird@gmail.com>
2021-03-25 23:20:00 +0000

    53 Added stubs

---
+++ 53-maximum_subarray.java
@@ -0,0 +1,12 @@
+class Solution {
+    /**
+     * Given an integer array nums, find the contiguous subarray (containing
+     * at least one number) which has the largest sum and return its sum.
+     * Constraints:
+     * 1 <= nums.length <= 3 * 104
+     * -105 <= nums[i] <= 105
+     */
+    public int maxSubArray(int[] nums) {
+
+    }
+}
---
+++ 53-maximum_subarray.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # Given an integer array nums, find the contiguous subarray (containing
+        # at least one number) which has the largest sum and return its sum.
+        # Constraints:
+        # 1 <= nums.length <= 3 * 104
+        # -105 <= nums[i] <= 105
