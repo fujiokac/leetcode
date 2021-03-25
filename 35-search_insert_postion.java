c8bbe5a020a002098b1f208ae43da99c5f8c63f9
fujiokac <vaguelyweird@gmail.com>
2021-03-25 22:05:08 +0000

    35 added java stub

---
+++ 35-search_insert_postion.java
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * Given a sorted array of distinct integers and a target value, return the index if the target is found.
+     * If not, return the index where it would be if it were inserted in order.
+     *
+     * Constraints:
+     * 1 <= nums.length <= 104
+     * -104 <= nums[i] <= 104
+     * nums contains distinct values sorted in ascending order.
+     * -104 <= target <= 104
+     */
+    public int searchInsert(int[] nums, int target) {
+
+    }
+}
