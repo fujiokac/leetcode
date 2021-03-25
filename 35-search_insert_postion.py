180fdf5f8968e6fd3e4df0130807a92f90a0b4cf
fujiokac <vaguelyweird@gmail.com>
2021-03-25 20:05:52 +0000

    super basic solution

---
+++ 35-search_insert_postion.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if nums.count(target) == 0:
+            nums.append(target)
+            nums.sort()
+        return nums.index(target)
