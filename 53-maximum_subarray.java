4c029074b41cd78deb3bc489d75b20a1082b592d
fujiokac <vaguelyweird@gmail.com>
2021-03-26 21:09:31 +0000

    53 python forgot to remove old code

--- 53-maximum_subarray.java
+++ 53-maximum_subarray.java
@@ -6,7 +6,23 @@ class Solution {
      * 1 <= nums.length <= 3 * 104
      * -105 <= nums[i] <= 105
      */
-    public int maxSubArray(int[] nums) {
+    public int maxSubArray(int[] nums) {       
+        if nums.length == 0:
+            return self.MIN
+        if nums.length == 1:
+            return nums[0]
+        currentSum = nums[0]
+        # this variation runs slower -- test case dependent?
+        # maxSum = currentSum
+        maxSum = max(currentSum, sum(nums))
 
+        for i in range(1, nums.length):
+            if currentSum < 0:
+                currentSum = 0
+            currentSum += nums[i]
+            if currentSum > maxSum:
+                maxSum = currentSum
+
+        return maxSum
     }
 }
--- 53-maximum_subarray.py
+++ 53-maximum_subarray.py
@@ -1,13 +1,10 @@
 class Solution:
-    # MIN = -math.pow(10,5)
     def maxSubArray(self, nums: List[int]) -> int:
         # Given an integer array nums, find the contiguous subarray (containing
         # at least one number) which has the largest sum and return its sum.
         # Constraints:
         # 1 <= nums.length <= 3 * 104
         # -105 <= nums[i] <= 105
-        if len(nums) == 0:
-            return self.MIN
         if len(nums) == 1:
             return nums[0]
         currentSum = nums[0]
