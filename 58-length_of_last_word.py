b65da27082117854526992a5fb70911c082503bf
fujiokac <vaguelyweird@gmail.com>
2021-03-27 08:07:53 +0000

    58 initializing stubs

---
+++ 58-length_of_last_word.py
@@ -0,0 +1,7 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        # Given a string s consists of some words separated by spaces,
+        # return the length of the last word in the string.
+        # If the last word does not exist, return 0.
+
+        # A word is a maximal substring consisting of non-space characters only.
