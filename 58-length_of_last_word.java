7f573a91e41893079355059eb1211f9f3e7d65ff
fujiokac <vaguelyweird@gmail.com>
2021-03-27 08:24:04 +0000

    58 java solution

--- 58-length_of_last_word.java
+++ 58-length_of_last_word.java
@@ -1,5 +1,20 @@
 class Solution {
+    /**
+     * Given a string s consists of some words separated by spaces,
+     * return the length of the last word in the string.
+     * If the last word does not exist, return 0.
+     *
+     * A word is a maximal substring consisting of non-space characters only.
+     */
     public int lengthOfLastWord(String s) {
-
+        int count = 0;
+        for (int i = s.length() -1; i >= 0; i--) {
+            if (s.charAt(i) == ' ') {
+                if (count > 0) break;
+            }
+            else
+                count++;
+        }
+        return count;
     }
 }
