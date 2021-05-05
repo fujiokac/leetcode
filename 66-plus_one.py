da47fd4dd194dd5e05a57f8239c6a870d12e9be1
fujiokac <vaguelyweird@gmail.com>
2021-05-05 10:15:14 +0000

    66 improving logic flow

--- 66-plus_one.py
+++ 66-plus_one.py
@@ -1,10 +1,9 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         for i, d in enumerate(reversed(digits)):
-            if d < 9:
+            if d == 9:
+                digits[-i-1] = 0
+            else:
                 digits[-i-1] += 1;
-                break
-            digits[-i-1] = 0
-            if i == len(digits) -1:
-                digits.insert(0,1)
-        return digits
+                return digits
+        return [1] + digits
